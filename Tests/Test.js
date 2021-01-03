9a5b29a4d5c43a91ae81b9d5624a68c0d1973129
Victor Monzonis Curos <victor.monzonis@gmail.com>
2021-01-03 12:16:06 +0100

    make test run

--- Tests/Test.js
+++ Tests/Test.js
@@ -1,5 +1,7 @@
-var assert = require('assert');
-var sut = require(".././sendroles");
+console.log(__dirname);
+
+var assert = require('chai').assert;
+var sut = require('../sendroles.js');
 
 describe('SendRoles', function() {
   describe('#Shuffle()', function() {
@@ -7,15 +9,57 @@ describe('SendRoles', function() {
       let initialArray = [1,2,3,4,5,6,7,8,9];
       let result = sut.shuffleArray(initialArray);
 
-      const reversed = initialArray.reverse(); // change after the shuffle is the reverse its almost 0 == 1 / Fact(9)
+      const reversed = [...initialArray].reverse(); // change after the shuffle is the reverse its almost 0 == 1 / Fact(9)
 
       let found = false;
       for(i = 0; i < reversed.length ;i++)
       {
         if(reversed[i] + result[i] != initialArray[0] + initialArray[initialArray.length -1])
-          found + true; // Because Gauss formula Sum of N first integers
+          found = true; // Because Gauss formula Sum of N first integers
       }
-      assert.match(true, found);
+      assert.equal(true, found);
+    });
+  });
+
+  describe('#AssignCharacters and enrich them', function() {
+    it('The resulting array after shuffle should not keep same order as the initial one', function() {
+      let gameSetup = [
+        {
+            "Name":"GoodGuy[x]",
+            "Role":"GoodGuy",
+            "Number":"4"
+        },
+        {
+            "Name":"BadGuy[x]",
+            "Role":"BadGuy",
+            "Number":"2"
+        },
+        {
+            "Name":"Merlin",
+            "Role":"GoodGuy",
+            "Number":"1",
+            "KnowRoles":["BadGuy"]
+        }
+    ];
+
+    let shuffledEmails = [
+      {"mail":"friendsEmails1@gmail.com", "aka":"FriendsNames1"},
+      {"mail":"friendsEmails2@gmail.com", "aka":"FriendsNames2"},
+      {"mail":"friendsEmails3@gmail.com", "aka":"FriendsNames3"},
+      {"mail":"friendsEmails4@gmail.com", "aka":"FriendsNames4"},
+      {"mail":"friendsEmails5@gmail.com", "aka":"FriendsNames5"},
+      {"mail":"friendsEmails6@gmail.com", "aka":"FriendsNames6"},
+      {"mail":"friendsEmails7@gmail.com", "aka":"FriendsNames7"}
+    ];
+
+    let characters = sut.assignCharacters(gameSetup, shuffledEmails);
+      assert.equal(shuffledEmails.length, characters.length);
+
+    characters = sut.enrichCharacters(characters);
+
+    let merlin = characters.filter(e => e.Name == 'Merlin')[0];
+    assert.isTrue(merlin.EnrichedInfo.length > 0);
+    assert.equal(merlin.KnowRoles.length, 1);    
     });
   });
 });
